f026a8e2978503792115164750e88dd5b89862b6
Tabor Wondimu <csmajortabor@gmail.com>
2022-02-27 09:00:53 -0800

    Finished Exercise

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace StaticExercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var fahrenheight = TempConvertor.CelsiusToFahrenheit(20);
+            var celsius = TempConvertor.FahrenheitToCelsius(68);
+
+            Console.WriteLine($"F is {fahrenheight}, C is {celsius}");
+        }
+    }
+}
